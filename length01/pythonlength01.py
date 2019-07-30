dfeca80875b92fd5d879853f4aa5dc3d24fc528c
Brad Luckett <brad.w.luckett@verizon.com>
2019-07-30 11:00:19 -0400

    more updates

---
+++ length01/pythonlength01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Demonstrate len() method   
+          
+# Length of below string is 8 
+string = "ZeroWing" 
+print(len(string)) 
+          
+# Length of below string is 30 
+string = "All your base are belong to us" 
+print(len(string)) 
