3e021c919b693d739a5763eb9b1a85acc696d921
Brad Luckett <brad.w.luckett@verizon.com>
2019-07-30 09:27:51 -0400

    todays update

---
+++ challenge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+columbus = ['Nina', 'Pinta', 'Santa-Maria', 3 ]
+
+#print("Columbus had" + str(columbus[3]) + "ships: the " + columbus[0] + ", the " + columbus[1] + ", and the " + columbus[2] + ".")) 
+
+print(f"Columbus had {columbus[3]} ships: the {columbus[0]}, the {columbus[1]}, and the {columbus[2]}.")
